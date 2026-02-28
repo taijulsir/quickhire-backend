74a2b86439f3864e597d8807ca3110754e56d69a
taijulsir <princetaijul123@gmail.com>
2026-02-28 23:26:47 +0600

    refactor: configure CORS policies and clean up seeders

--- src/app.js
+++ src/app.js
@@ -1,19 +1,37 @@
-const express = require('express');
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
-const { errorHandler } = require('./middlewares');
-const { jobRoutes, applicationRoutes, adminRoutes } = require('./routes');
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { errorHandler } from './middlewares/index.js';
+import { jobRoutes, applicationRoutes, adminRoutes } from './routes/index.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
-  credentials: true
+  origin: function (origin, callback) {
+    callback(null, true);
+  },
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
+
+// Cross-Origin-Resource-Policy for images to fix 403
+app.use((req, res, next) => {
+  res.setHeader("Cross-Origin-Resource-Policy", "cross-origin");
+  next();
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+// Serve static images
+app.use("/images", express.static(path.join(__dirname, "../public/images")));
 
 app.get('/api/health', (req, res) => {
   res.json({ success: true, message: 'QuickHire API is running' });
@@ -29,4 +47,4 @@ app.use((req, res) => {
 
 app.use(errorHandler);
 
-module.exports = app;
+export default app;
