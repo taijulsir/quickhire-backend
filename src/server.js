144b10acac7f1957809326fb238b5b9d5936d06a
taijulsir <princetaijul123@gmail.com>
2026-02-28 23:27:43 +0600

    chore: setup new server instance with ES native mappings

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
-require('dotenv').config();
-const app = require('./app');
-const connectDB = require('./config/db');
+import dotenv from 'dotenv';
+dotenv.config();
+import app from './app.js';
+import connectDB from './config/db.js';
 
 const PORT = process.env.PORT || 5000;
 
